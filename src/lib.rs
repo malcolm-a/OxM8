7d8e39246329c8e22d8642c2f1565c2f974cb915
malcolm-a <aridoryjr@icloud.com>
2025-09-21 13:40:15 +0200

    feat(lib.rs): added game module and reorganized modules order

--- src/lib.rs
+++ src/lib.rs
@@ -1,12 +1,15 @@
 pub mod board;
 pub mod fen;
+pub mod game;
+pub mod moves;
 pub mod piece;
 pub mod util;
-pub mod moves;
 
 #[cfg(test)]
 mod tests;
 
 pub use board::Board;
-pub use piece::{Color, Piece};
+pub use game::ChessGame;
 pub use moves::Moves;
+pub use piece::{Color, Piece};
+
